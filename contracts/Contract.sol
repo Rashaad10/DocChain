6f47d97a865020b8fff976dcdb1861340db99176
Rashaad10 <baigrashaad@gmail.com>
2024-01-27 02:19:07 +0530

    first commit

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -1,6 +1,81 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+// SPDX-License-Identifier: Unlicensed
 
-contract MyContract {
-    constructor() {}
+pragma solidity ^0.8.7;
+
+contract ClaimsContract {
+    
+    // Define a claim structure
+    struct Claim {
+        string field;
+        string value;
+        address issuer; // address of the issuer
+    }
+
+    // Define a user structure
+    struct User {
+        bool exists;
+        Claim[] claims; // Array of claims
+    }
+
+    uint public totalUser;
+    // Mapping from an address to a user
+    mapping(address => User) public users;
+
+    // Function to add a new user
+    function addUser(address userAddress) public {
+        require(!users[userAddress].exists, "User already exists");
+        users[userAddress].exists = true;
+        totalUser++;
+
+    }
+
+    //get total number of User
+    function getNoOfUser() public view returns(uint){
+        return totalUser;
+    }
+    
+    // Function to make a claim about a user
+    function makeClaim(address userAddress, string memory field, string memory value) public {
+        require(users[userAddress].exists, "User does not exist");
+
+        // Create a new claim
+        Claim memory newClaim = Claim({
+            field: field,
+            value: value,
+            issuer: msg.sender
+        });
+
+        // Add the claim to the user's claims
+        users[userAddress].claims.push(newClaim);
+    }
+
+    // Function to get the number of claims a user has
+    function getClaimsCount(address userAddress) public view returns (uint) {
+        require(users[userAddress].exists, "User does not exist");
+        return users[userAddress].claims.length;
+    }
+
+    //function to return all the claims of a user
+    
+    // Function to return all claims of a user
+    function getAllClaims(address userAddress) public view returns (string[] memory, string[] memory, address[] memory) {
+        require(users[userAddress].exists, "User does not exist");
+
+        uint claimCount = users[userAddress].claims.length;
+
+        // Initialize arrays to hold the claim data
+        string[] memory fields = new string[](claimCount);
+        string[] memory values = new string[](claimCount);
+        address[] memory issuers = new address[](claimCount);
+
+        // Populate the arrays
+        for (uint i = 0; i < claimCount; i++) {
+            Claim storage claim = users[userAddress].claims[i];
+            fields[i] = claim.field;
+            values[i] = claim.value;
+            issuers[i] = claim.issuer;
+        }
+
+        return (fields, values, issuers);
+    }   
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -33,6 +33,14 @@ module.exports = {
   },
   solidity: {
     version: "0.8.17",
+    defaultNetwork: 'sepolia', // use your own testnet 
+    networks: {
+      hardhat: {},
+      sepolia: {    // use your own testnet url
+        url: 'https://rpc.ankr.com/eth_sepolia',
+        accounts: [`0x${process.env.PRIVATE_KEY}`]
+      }
+    },
     settings: {
       optimizer: {
         enabled: true,
